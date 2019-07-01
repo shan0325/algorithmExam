0b474590dd4c0e33f8e0b6cf2ea7a9eed6298064
dev1 <dev1@AN2>
2019-07-01 18:04:16 +0900

    .

--- src/main/java/com/algorithm/codingTest/kit/alignment/ex02/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/alignment/ex02/Solution.java
@@ -1,57 +1,48 @@
 package com.algorithm.codingTest.kit.alignment.ex02;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Solution {
-
+	
 	public String solution(int[] numbers) { 
-//		String answer = Arrays.toString(numbers).replaceAll("[^0-9]", "");
-//		System.out.println("answer : " + answer);
-//		numbers = Stream.of(answer.split("")).sorted((a, b) -> b.compareTo(a)).mapToInt(Integer::parseInt).toArray();
-//		return Arrays.toString(numbers).replaceAll("[^0-9]", "");
-		
-		String answer = "";
-		
-		Queue<String> queue = new LinkedList<String>();
-		for (int number : numbers) {
-			queue.add(String.valueOf(number));
-		}
-		System.out.println(queue);
-		
-		int size = numbers.length;
-		int index = 0;
-		int index2 = 0;
-		while(true) {
-			String numStr = queue.poll();
-			System.out.println("numStr : " + numStr);
-			
-			while(true) {
-				String numStr2 = queue.poll();
+		List<String> list = Arrays.stream(numbers).mapToObj(String::valueOf).sorted((a, b) -> (b+a).compareTo(a+b)).collect(Collectors.toList());
+		return list.get(0).equals("0") ? "0" : list.toString().replaceAll("[^0-9]", "");
+	}
+	
+	public String solution2(int[] numbers) {
+		for(int i = 0; i < numbers.length; i++) {
+			for(int j = i+1; j < numbers.length; j++) {
+				int num = numbers[i];
+				int num2 = numbers[j];
+				String numStr = String.valueOf(num) + String.valueOf(num2);
+				String numStr2 = String.valueOf(num2) + String.valueOf(num);
+				System.out.println("numStr : " + numStr + " // numStr2 : " + numStr2);
 				
-				
-				
-				break;
+				if(numStr.compareTo(numStr2) < 0) {
+					numbers[i] = num2;
+					numbers[j] = num;
+				}
 			}
-			
-			break;
 		}
 		
-		return answer;
+		List<String> list = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.toList());
+		return list.get(0).equals("0") ? "0" : list.toString().replaceAll("[^0-9]", "");
 	}
 	
 	public static void main(String[] args) {
-		int[] numbers = {6, 10, 2}; //6210
+//		int[] numbers = {6, 10, 2}; //6210
 //		6102, 6210, 1062, 1026, 2610,2106
 //		61023, 62310, 63102, 10236, 10362, 10623 
 		
 		
-//		int[] numbers = {3, 30, 34, 5, 9}; //9534330
+		int[] numbers = {3, 30, 34, 5, 9}; //9534330
+//		int[] numbers = {12, 121}; //12121
+//		int[] numbers = {0, 0, 0, 0}; //0
 		
 		Solution solution = new Solution();
-		String answer = solution.solution(numbers);
+		String answer = solution.solution2(numbers);
 		System.out.println(answer);
 	}
 }
