690fab3baa9e557d238ba688e3139e1478d606d2
dev1 <shan0325@naver.com>
2019-07-24 14:14:14 +0900

    .

--- src/main/java/com/algorithm/codingTest/kit/completeSearch/ex01/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/completeSearch/ex01/Solution.java
@@ -1,66 +1,56 @@
 package com.algorithm.codingTest.kit.completeSearch.ex01;
 
-import java.util.OptionalInt;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Solution {
 
 	public int[] solution(int[] answers) {
-		int[] answer = {};
-		
-		int[] answer1 = {1,2,3,4,5};
-		int[] answer2 = {2,1,2,3,2,4,2,5};
-		int[] answer3 = {3,3,1,1,2,2,4,4,5,5};
-		
-		int[] a = new int[answers.length];
-		int[] b = new int[answers.length];
-		int[] c = new int[answers.length];
-		
-		int aCnt = 0;
-		int bCnt = 0;
-		int cCnt = 0;
-		
+		int[] user1 = {1,2,3,4,5};
+		int[] user2 = {2,1,2,3,2,4,2,5};
+		int[] user3 = {3,3,1,1,2,2,4,4,5,5};
+		int[] userCnt = new int[3];
+
 		for(int i = 0; i < answers.length; i++) {
-			if(i < 5) {
-				a[i] = answer1[i];
-			} else {
-				a[i] = answer1[i / 5];
+			if(answers[i] == user1[i % 5]) {
+				userCnt[0]++; 
 			}
-			
-			if(i < 8) {
-				b[i] = answer2[i];
-			} else {
-				b[i] = answer2[i / 8];
+			if(answers[i] == user2[i % 8]) {
+				userCnt[1]++;
 			}
-			
-			if(i < 10) {
-				c[i] = answer3[i];
-			} else {
-				c[i] = answer3[i / 10];
+			if(answers[i] == user3[i % 10]) {
+				userCnt[2]++;
 			}
 		}
 		
-		for(int i = 0; i < answers.length; i++) {
-			if(answers[i] == a[i]) aCnt++;
-			if(answers[i] == b[i]) bCnt++;
-			if(answers[i] == c[i]) cCnt++;
+		IntStream.of(userCnt).forEach(cnt -> System.out.print(cnt +" "));
+		System.out.println();
+		
+		int maxCnt = IntStream.of(userCnt).max().orElse(0);
+		
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < userCnt.length; i++) {
+			if(maxCnt == userCnt[i]) {
+				list.add(i+1);
+			}
 		}
 		
-//		if(aCnt == bCnt && aCnt == cCnt && bCnt == cCnt) {
-//			int[] answer = {1,2,3};
-//			return answer;
-//		}
-		
-		
+		int[] answer = list.stream().mapToInt(i -> i).toArray();
+		Arrays.sort(answer);
 		return answer;
 	}
 	
 	public static void main(String[] args) {
-		int[] answers = {1,2,3,4,5};
+//		int[] answers = {1,2,3,4,5};
+//		int[] answers = {1,3,2,4,2};
+		int[] answers = {9,9,9,9,9,9,9,3};
 		
 		Solution solution = new Solution();
 		int[] answer = solution.solution(answers);
 		
 		IntStream.of(answer).forEach((a) -> System.out.print(a + " "));
 	}
+	
 }
