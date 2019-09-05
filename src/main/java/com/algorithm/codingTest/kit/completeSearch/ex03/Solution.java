0e76d96582caefc2b8e63352af1e2e9456d10b88
dev1 <shan0325@naver.com>
2019-09-05 17:58:11 +0900

    .

--- src/main/java/com/algorithm/codingTest/kit/completeSearch/ex03/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/completeSearch/ex03/Solution.java
@@ -1,21 +1,16 @@
 package com.algorithm.codingTest.kit.completeSearch.ex03;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Solution {
 
 	public int solution(int[][] baseball) {
-		int answer = 0;
-		
 		int[] numbers = {1,2,3,4,5,6,7,8,9};
 		Set<String> possibleNumbers = new HashSet<String>();
 		doPermutation(numbers, 9, 3, 0, possibleNumbers);
 		System.out.println(possibleNumbers);
 		
-		Set<String> list = new HashSet<String>();
 		for(int i = 0; i < baseball.length; i++) {
 			String userNum = String.valueOf(baseball[i][0]);
 			String firstNum = userNum.substring(0, 1);
@@ -25,67 +20,124 @@ public class Solution {
 			int strike = baseball[i][1];
 			int ball = baseball[i][2];
 			System.out.println("userNum : " + userNum + " / strike : " + strike + " / ball : " + ball);
-		
+
+			Set<String> set = new HashSet<String>();
 			for(String possibleNum : possibleNumbers) {
 				String firstNum2 = possibleNum.substring(0, 1);
 				String secondNum2 = possibleNum.substring(1, 2);
 				String third2 = possibleNum.substring(2);
 				
-				if(strike == 1) {
-					if(firstNum.equals(firstNum2)) {
-						list.add(possibleNum);
+				if(!userNum.equals(possibleNum)) {
+					if(strike == 0) {
+						if(ball == 0) {
+							if(possibleNum.contains(firstNum) || possibleNum.contains(secondNum) || possibleNum.contains(third)) {
+								possibleNumbers.remove(possibleNum);
+							}
+						}
+						else if(ball == 1) {
+							if(!firstNum.equals(firstNum2) && possibleNum.contains(firstNum)) {
+								set.add(possibleNum);
+							}
+							if(!secondNum.equals(secondNum2) && possibleNum.contains(secondNum)) {
+								set.add(possibleNum);
+							}
+							if(!third.equals(third2) && possibleNum.contains(third)) {
+								set.add(possibleNum);
+							}
+						}
+						else if(ball == 2) {
+							if((secondNum + firstNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							if((third + secondNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							if((third + secondNum).equals(secondNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							if((third + firstNum).equals(secondNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							if((third + firstNum).equals(firstNum2 + third2)) {
+								set.add(possibleNum);
+							}
+						}
+						else if(ball == 3) {
+							if((secondNum + third + firstNum).equals(possibleNum)) {
+								set.add(possibleNum);
+							}
+							if((third + firstNum + secondNum).equals(possibleNum)) {
+								set.add(possibleNum);
+							}
+						}
 					}
-					if(secondNum.equals(secondNum2)) {
-						list.add(possibleNum);
-					}
-					if(third.equals(third2)) {
-						list.add(possibleNum);
-					}
-				} else if(strike == 2) {
-					if((firstNum + secondNum).equals(firstNum2 + secondNum2)) {
-						list.add(possibleNum);
-					}
-					if((secondNum + third).equals(secondNum2 + third2)) {
-						list.add(possibleNum);
-					}
-					if((firstNum + third).equals(firstNum2 + third2)) {
-						list.add(possibleNum);
-					}
-				}
-				
-				if(ball == 1) {
-					if(possibleNum.contains(firstNum)) {
-						list.add(possibleNum);
-					}
-					if(possibleNum.contains(secondNum)) {
-						list.add(possibleNum);
-					}
-					if(possibleNum.contains(third)) {
-						list.add(possibleNum);
-					}
-				} else if(ball == 2) {
-					if((secondNum + firstNum).equals(secondNum2 + firstNum2)) {
-						list.add(possibleNum);
-					}
-					if((third + secondNum).equals(third2 + secondNum2)) {
-						list.add(possibleNum);
-					}
-					if((third + firstNum).equals(third2 + firstNum2)) {
-						list.add(possibleNum);
-					}
-				} else if(ball == 3) {
-					if((third + firstNum + secondNum).equals(third2 + firstNum2 + secondNum2)){
-						list.add(possibleNum);
-					}
-					if((secondNum + third + firstNum).equals(secondNum2 + third2 + firstNum2)) {
-						list.add(possibleNum);
+					else if(strike == 1) {
+						if(ball == 0) {
+							if(firstNum.equals(firstNum2) && !possibleNum.contains(secondNum) && !possibleNum.contains(third)) {
+								set.add(possibleNum);
+							}
+							if(secondNum.equals(secondNum2) && !possibleNum.contains(firstNum) && !possibleNum.contains(third)) {
+								set.add(possibleNum);
+							}
+							if(third.equals(third2) && !possibleNum.contains(firstNum) && !possibleNum.contains(secondNum)) {
+								set.add(possibleNum);
+							}
+						}
+						else if(ball == 1) {
+							if((firstNum + third).equals(firstNum2 + secondNum2) && !possibleNum.contains(secondNum)) {
+								set.add(possibleNum);
+							}
+							if((secondNum + firstNum).equals(secondNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							if((third + secondNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							if((firstNum + third).equals(secondNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							if((secondNum + third).equals(firstNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							
+						} 
+						else if(ball == 2) {
+							if((firstNum + third + secondNum).equals(possibleNum)) {
+								set.add(possibleNum);
+							}
+							if((third + secondNum + firstNum).equals(possibleNum)) {
+								set.add(possibleNum);
+							}
+							if((secondNum + firstNum + third).equals(possibleNum)) {
+								set.add(possibleNum);
+							}
+						}
+					} 
+					else if(strike == 2) {
+						if(ball == 0) {
+							if((firstNum + secondNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							if((secondNum + third).equals(secondNum2 + third2)) {
+								set.add(possibleNum);
+							}
+							if((firstNum + third).equals(firstNum2 + third2)) {
+								set.add(possibleNum);
+							}
+						}
 					}
 				}
 			}
-			System.out.println("list : " + list);
+			System.out.println("set : " + set);
+			
+			possibleNumbers.clear();
+			for(String num : set) {
+				possibleNumbers.add(num);
+			}
 		}
 		
-		return answer;
+		System.out.println("possibleNumbers : " + possibleNumbers);
+		return possibleNumbers.size();
 	}
 	
 	public void doPermutation(int[] numbers, int n, int r, int target, Set<String> possibleNumbers) {
