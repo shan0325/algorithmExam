4f4608420676704d339d9406bd31e26d154bacb7
dev1 <shan0325@naver.com>
2019-09-06 17:55:46 +0900

    .

--- src/main/java/com/algorithm/codingTest/kit/completeSearch/ex03/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/completeSearch/ex03/Solution.java
@@ -2,7 +2,48 @@ package com.algorithm.codingTest.kit.completeSearch.ex03;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
+/**
+ * 
+ * @author dev1
+ * 
+ * 문제 설명
+숫자 야구 게임이란 2명이 서로가 생각한 숫자를 맞추는 게임입니다. 게임해보기
+
+각자 서로 다른 1~9까지 3자리 임의의 숫자를 정한 뒤 서로에게 3자리의 숫자를 불러서 결과를 확인합니다. 그리고 그 결과를 토대로 상대가 정한 숫자를 예상한 뒤 맞힙니다.
+
+* 숫자는 맞지만, 위치가 틀렸을 때는 볼
+* 숫자와 위치가 모두 맞을 때는 스트라이크
+* 숫자와 위치가 모두 틀렸을 때는 아웃
+예를 들어, 아래의 경우가 있으면
+
+A : 123
+B : 1스트라이크 1볼.
+A : 356
+B : 1스트라이크 0볼.
+A : 327
+B : 2스트라이크 0볼.
+A : 489
+B : 0스트라이크 1볼.
+이때 가능한 답은 324와 328 두 가지입니다.
+
+질문한 세 자리의 수, 스트라이크의 수, 볼의 수를 담은 2차원 배열 baseball이 매개변수로 주어질 때, 가능한 답의 개수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+질문의 수는 1 이상 100 이하의 자연수입니다.
+baseball의 각 행은 [세 자리의 수, 스트라이크의 수, 볼의 수] 를 담고 있습니다.
+
+입출력 예
+baseball													return
+[[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]		2
+
+입출력 예 설명
+문제에 나온 예와 같습니다.
+
+
+ *
+ */
 public class Solution {
 
 	public int solution(int[][] baseball) {
@@ -31,34 +72,46 @@ public class Solution {
 					if(strike == 0) {
 						if(ball == 0) {
 							if(possibleNum.contains(firstNum) || possibleNum.contains(secondNum) || possibleNum.contains(thirdNum)) {
-								possibleNumbers.remove(possibleNum);
+								set.add(possibleNum);
 							}
 						}
 						else if(ball == 1) {
-							if(!firstNum.equals(firstNum2) && possibleNum.contains(firstNum)) {
+							if(!possibleNum.contains(secondNum) && !possibleNum.contains(thirdNum) && (firstNum.equals(secondNum2) || firstNum.equals(thirdNum2))) {
 								set.add(possibleNum);
 							}
-							if(!secondNum.equals(secondNum2) && possibleNum.contains(secondNum)) {
+							else if(!possibleNum.contains(firstNum) && !possibleNum.contains(thirdNum) && (secondNum.equals(firstNum2) || secondNum.equals(thirdNum2))) {
 								set.add(possibleNum);
 							}
-							if(!thirdNum.equals(thirdNum2) && possibleNum.contains(thirdNum)) {
+							else if(!possibleNum.contains(firstNum) && !possibleNum.contains(secondNum) && (thirdNum.equals(firstNum2) || thirdNum.equals(secondNum2))) {
 								set.add(possibleNum);
 							}
 						}
 						else if(ball == 2) {
-							if((secondNum + firstNum).equals(firstNum2 + secondNum2)) {
+							if(!possibleNum.contains(thirdNum) && (firstNum + secondNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + secondNum).equals(firstNum2 + secondNum2)) {
+							else if(!possibleNum.contains(thirdNum) && (secondNum + firstNum).equals(firstNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + secondNum).equals(secondNum2 + thirdNum2)) {
+							else if(!possibleNum.contains(thirdNum) && (secondNum + firstNum).equals(firstNum2 + secondNum2)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + firstNum).equals(secondNum2 + thirdNum2)) {
+							else if(!possibleNum.contains(firstNum) && (thirdNum + secondNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + firstNum).equals(firstNum2 + thirdNum2)) {
+							else if(!possibleNum.contains(firstNum) && (thirdNum + secondNum).equals(firstNum2 + thirdNum2)) {
+								set.add(possibleNum);
+							}
+							else if(!possibleNum.contains(firstNum) && (secondNum + thirdNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							else if(!possibleNum.contains(secondNum) && (thirdNum + firstNum).equals(firstNum2 + thirdNum2)) {
+								set.add(possibleNum);
+							}
+							else if(!possibleNum.contains(secondNum) && (thirdNum + firstNum).equals(firstNum2 + secondNum2)) {
+								set.add(possibleNum);
+							}
+							else if(!possibleNum.contains(secondNum) && (thirdNum + firstNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
 						}
@@ -66,37 +119,40 @@ public class Solution {
 							if((secondNum + thirdNum + firstNum).equals(possibleNum)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + firstNum + secondNum).equals(possibleNum)) {
+							else if((thirdNum + firstNum + secondNum).equals(possibleNum)) {
 								set.add(possibleNum);
 							}
 						}
 					}
 					else if(strike == 1) {
 						if(ball == 0) {
-							if(firstNum.equals(firstNum2) && !possibleNum.contains(secondNum) && !possibleNum.contains(thirdNum)) {
+							if(!possibleNum.contains(secondNum) && !possibleNum.contains(thirdNum) && firstNum.equals(firstNum2)) {
 								set.add(possibleNum);
 							}
-							if(secondNum.equals(secondNum2) && !possibleNum.contains(firstNum) && !possibleNum.contains(thirdNum)) {
+							else if(!possibleNum.contains(firstNum) && !possibleNum.contains(thirdNum) && secondNum.equals(secondNum2)) {
 								set.add(possibleNum);
 							}
-							if(thirdNum.equals(thirdNum2) && !possibleNum.contains(firstNum) && !possibleNum.contains(secondNum)) {
+							else if(!possibleNum.contains(firstNum) && !possibleNum.contains(secondNum) && thirdNum.equals(thirdNum2)) {
 								set.add(possibleNum);
 							}
 						}
 						else if(ball == 1) {
-							if((firstNum + thirdNum).equals(firstNum2 + secondNum2) && !possibleNum.contains(secondNum)) {
+							if(!possibleNum.contains(secondNum) && (firstNum + thirdNum).equals(firstNum2 + secondNum2)) {
 								set.add(possibleNum);
 							}
-							if((secondNum + firstNum).equals(secondNum2 + thirdNum2) && !possibleNum.contains(thirdNum)) {
+							else if(!possibleNum.contains(thirdNum) && (firstNum + secondNum).equals(firstNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + secondNum).equals(firstNum2 + secondNum2) && !possibleNum.contains(firstNum)) {
+							else if(!possibleNum.contains(firstNum) && (thirdNum + secondNum).equals(firstNum2 + secondNum2)) {
 								set.add(possibleNum);
 							}
-							if((firstNum + thirdNum).equals(secondNum2 + thirdNum2) && !possibleNum.contains(secondNum)) {
+							else if(!possibleNum.contains(thirdNum) && (secondNum + firstNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((secondNum + thirdNum).equals(firstNum2 + thirdNum2) && !possibleNum.contains(firstNum)) {
+							else if(!possibleNum.contains(firstNum) && (secondNum + thirdNum).equals(firstNum2 + thirdNum2)) {
+								set.add(possibleNum);
+							}
+							else if(!possibleNum.contains(secondNum) && (firstNum + thirdNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
 						} 
@@ -104,10 +160,10 @@ public class Solution {
 							if((firstNum + thirdNum + secondNum).equals(possibleNum)) {
 								set.add(possibleNum);
 							}
-							if((thirdNum + secondNum + firstNum).equals(possibleNum)) {
+							else if((thirdNum + secondNum + firstNum).equals(possibleNum)) {
 								set.add(possibleNum);
 							}
-							if((secondNum + firstNum + thirdNum).equals(possibleNum)) {
+							else if((secondNum + firstNum + thirdNum).equals(possibleNum)) {
 								set.add(possibleNum);
 							}
 						}
@@ -117,10 +173,10 @@ public class Solution {
 							if((firstNum + secondNum).equals(firstNum2 + secondNum2)) {
 								set.add(possibleNum);
 							}
-							if((secondNum + thirdNum).equals(secondNum2 + thirdNum2)) {
+							else if((secondNum + thirdNum).equals(secondNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
-							if((firstNum + thirdNum).equals(firstNum2 + thirdNum2)) {
+							else if((firstNum + thirdNum).equals(firstNum2 + thirdNum2)) {
 								set.add(possibleNum);
 							}
 						}
@@ -129,9 +185,15 @@ public class Solution {
 			}
 			System.out.println("set : " + set);
 			
-			possibleNumbers.clear();
-			for(String num : set) {
-				possibleNumbers.add(num);
+			if(strike == 0 && ball == 0) {
+				for(String num : set) {
+					possibleNumbers.remove(num);
+				}
+			} else {
+				possibleNumbers.clear();
+				for(String num : set) {
+					possibleNumbers.add(num);
+				}
 			}
 		}
 		
@@ -162,15 +224,62 @@ public class Solution {
 		arr[end] = temp;
 	}
 	
+	public int solution2(int[][] baseball) {
+		Stack<String> stack = new Stack<>();
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				for(int k = 1; k < 10; k++) {
+					if(i != j && j != k && i != k) {
+						stack.add(String.valueOf(i * 100 + j * 10 + k));
+					}
+				}
+			}
+		}
+		
+		Stack<String> temp = new Stack<>();
+		boolean flag = true;
+		while(!stack.isEmpty()) {
+			String num = stack.pop();
+			for(int i = 0; i < baseball.length; i++) {
+				int strike = strike(num, String.valueOf(baseball[i][0]));
+				int ball = ball(num, String.valueOf(baseball[i][0])) - strike;
+				System.out.println("num : " + num + " / baseball : " + baseball[i][0] + " / strike : " + strike + " / ball : " + ball);
+				
+				if(strike != baseball[i][1] || ball != baseball[i][2]) {
+					flag = false;
+				}
+			}
+			if(flag) {
+				temp.add(num);
+			}
+			flag = true;
+			System.out.println("----------------------------------------------------------");
+		}
+		System.out.println("temp : " + temp);
+		return temp.size();
+	}
+	
+	public int strike(String num, String target) {
+		int cnt = 0;
+		for(int i = 0; i < target.length(); i++) {
+			cnt = num.charAt(i) == target.charAt(i) ? cnt + 1 : cnt;
+		}
+		return cnt;
+	}
+	
+	public int ball(String num, String target) {
+		int cnt = 0;
+		for(int i = 0; i < target.length(); i++) {
+			cnt = num.contains(String.valueOf(target.charAt(i))) ? cnt + 1 : cnt;
+		}
+		return cnt;
+	}
+	
 	public static void main(String[] args) {
 		int[][] baseball = {{123, 1, 1},{356, 1, 0},{327, 2, 0},{489, 0, 1}};
 		
 		Solution solution = new Solution();
-		int answer = solution.solution(baseball);
+		int answer = solution.solution2(baseball);
 		System.out.println(answer);
 	}
 }
-
-
-//127
-//이때 가능한 답은 324와 328 두 가지입니다.
