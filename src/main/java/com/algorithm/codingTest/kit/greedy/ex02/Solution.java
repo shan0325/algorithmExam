4c4f1ec8f8b97ce902bf2ee993c8689f71eb8484
dev1 <shan0325@naver.com>
2019-09-27 18:05:17 +0900

    .

--- src/main/java/com/algorithm/codingTest/kit/greedy/ex02/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/greedy/ex02/Solution.java
@@ -4,37 +4,80 @@ public class Solution {
 
 	public int solution(String name) {
 		int answer = 0;
-		int ascStart = 65;
-		boolean back = false;
+		int loc = 0;
 		char[] names = name.toCharArray();
 		
-		for(int i = 0; i < names.length; i++) {
-			int seq = names[i] - ascStart;
-			
-			if(seq == 0 && i - 1 == 0) {
-				answer++;
-				back = true;
+		int index = 0;
+		while(true) {
+			int endCnt = 0;
+			for(int i = 0; i < names.length; i++) {
+				if(names[i] == '-') {
+					endCnt++;
+				}
 			}
+			if(endCnt == names.length) break;
 
-			if(i > 0 && !back) {
-				answer++;
-			}
-			
-			if(seq > 0) {
-				if(seq <= 12) {
-					answer += seq;
+			int front = 0;
+			int back = index;
+			for(int i = index; i < names.length; i++) {
+				int engIndex2 = names[i] - 65;
+				if(engIndex2 == 0) {
+					front++;
 				} else {
-					answer += 26 - seq;
+					break;
+				}
+			}
+			for(int i = names.length - 1; i >= index; i--) {
+				int engIndex2 = names[i] - 65;
+				if(engIndex2 == 0) {
+					back++;
+				} else {
+					break;
+				}
+			}
+			System.out.println("front : " + front + " / back : " + back);
+			
+			if(front == back) {
+				answer++;
+				names[index++] = '-';
+			} else if(front < back) {
+				answer += front;
+				for(int i = index; i < names.length; i++) {
+					int engIndex2 = names[i] - 65;
+					if(engIndex2 == 0) {
+						names[i++] = '-';
+					} else {
+						break;
+					}
+				}
+			} else {
+				answer += back;
+				for(int i = names.length - 1; i >= index; i--) {
+					int engIndex2 = names[i] - 65;
+					if(engIndex2 == 0) {
+						names[i--] = '-';
+					} else {
+						break;
+					}
 				}
 			}
 			
-			System.out.println(names[i] + " : " + seq + " / answer : " + answer);
+			int engIndex = names[index] - 65;
+			
+			if(engIndex <= 12) {
+				answer += engIndex;
+			} else {
+				answer += 26 - engIndex;
+			}
+			System.out.println(names[index] + " : " + engIndex + " / answer : " + answer);
+			
+			
 		}
 		return answer;
 	}
 	
 	public static void main(String[] args) {
-		String name = "JAN";
+		String name = "ABAAAAAAABA"; // 6
 		
 		Solution solution = new Solution();
 		int answer = solution.solution(name);
