83e7039d1ecf6106a0bf57fb24714148ae60385e
shan0325 <shan0325@naver.com>
2020-06-19 20:30:49 +0900

    h

--- src/main/java/com/algorithm/codingTest/kit/greedy/ex07/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/greedy/ex07/Solution.java
@@ -5,45 +5,15 @@ import java.util.Arrays;
 public class Solution {
 	
 	public int solution(int[] weight) {
-        int answer = 1;
-        int sum = Arrays.stream(weight).sum();
-        
-        for (int i = 0; i < weight.length; i++) {
-			for (int j = 0; j < weight.length; j++) {
-				if(weight[i] > weight[j]) {
-					int temp = weight[i];
-					weight[i] = weight[j];
-					weight[j] = temp;
-				}
+        Arrays.sort(weight);
+        int answer = weight[0] + 1;
+        for (int i = 1; i < weight.length; i++) {
+			if(answer >= weight[i]) {
+				answer += weight[i];
+				continue;
 			}
+			break;
 		}
-        
-        for (int i = 0; i < weight.length; i++) {
-			System.out.print(weight[i] + ",");
-		}
-        System.out.println();
-        
-        while(answer <= sum) {
-        	boolean isFound = false;
-        	
-        	int temp = answer;
-        	for (int i = 0; i < weight.length; i++) {
-        		if(temp - weight[i] >= 0) {
-        			temp -= weight[i];
-        			if(temp == 0) {
-        				isFound = true;
-        				break;
-        			}
-        		}
-			}
-        	
-        	if(!isFound) {
-        		break;
-        	} else {
-        		answer++;
-        	}
-        }
-        
         return answer;
     }
 
