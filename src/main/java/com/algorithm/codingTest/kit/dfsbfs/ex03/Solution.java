211ea969bb7beeea1467619bcde40ff2999c8d96
shan0325 <shan0325@naver.com>
2021-01-29 00:27:57 +0900

    home

--- src/main/java/com/algorithm/codingTest/kit/dfsbfs/ex03/Solution.java
+++ src/main/java/com/algorithm/codingTest/kit/dfsbfs/ex03/Solution.java
@@ -1,74 +1,50 @@
 package com.algorithm.codingTest.kit.dfsbfs.ex03;
 
-import java.util.Stack;
 
 public class Solution {
 	public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        
-        boolean isExist = false;
-        for (int i = 0; i < words.length; i++) {
-			if(words[i].equals(target)) {
-				isExist = true;
-				break;
-			}
-		}
-        if(!isExist) 
-        	return answer;
-        
-        Stack<String> stack = new Stack<>();
-        stack.push(begin);
-        
-        for (int i = 0; i < words.length; i++) {
-			if(stack.contains(target)) 
-				break;
-        	
-        	find(i, 0, stack, target, words);
-        	answer++;
-		}
-        System.out.println("stack : " + stack);
-        
-        return answer + 1;
+        int answer = words.length + 1;
+
+        answer = dfs(begin, target, words, new boolean[words.length], 0, words.length + 1, words.length);
+
+        return answer == words.length + 1 ? 0 : answer;
     }
-	
-	public void find(int index, int node, Stack<String> stack, String target, String[] words) {
-		
-		String word = words[index];
-		if(node >= word.length()) {
-			return;
-		}
-		
-		String compWord = word.substring(0, node) + word.substring(node + 1);
-		System.out.println("word : " + word + " // compWord : " + compWord);
-		
-		for (int i = 0; i < target.length(); i++) {
-			String compWord2 = target.substring(0, i) + target.substring(i + 1);
-			System.out.println("target : " + target + " // compWord2 : " + compWord2);
-			
-			if(compWord.equals(compWord2)) {
-				stack.push(target);
-				return;
+
+    public int dfs(String word, String target, String[] words, boolean[] visited, int n, int answer, int max) {
+		for (int i = 0; i < max; i++) {
+			if (!visited[i] && conversion(word, words[i])) {
+				System.out.println(n + " " + answer + " " + word + " " + words[i]);
+
+				if (words[i].equals(target)) {
+					return Math.min(answer, n + 1);
+				}
+
+				visited[i] = true;
+				int num = dfs(words[i], target, words, visited, n + 1, answer, max);
+				if (num < answer) answer = num;
+				visited[i] = true;
 			}
 		}
-		
-		String word2 = stack.peek();
-		for (int i = 0; i < word2.length(); i++) {
-			String compWord2 = word2.substring(0, i) + word2.substring(i + 1);
-			System.out.println("target : " + word2 + " // compWord2 : " + compWord2);
-			
-			if(compWord.equals(compWord2)) {
-				stack.push(word);
-				return;
+
+		return answer;
+	}
+
+	public boolean conversion(String w1, String w2) {
+		int tmp = 0;
+		for (int i = 0; i < w1.length(); i++) {
+			if (w1.charAt(i) != w2.charAt(i)) {
+				tmp++;
+				if (tmp > 1) return false;
 			}
 		}
-		
-		find(index, node + 1, stack, target, words);
+		return true;
 	}
 
 	public static void main(String[] args) {
 		String begin = "hit", target = "cog";
 		String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
-		
+		//String[] words = {"hot", "dot", "dog", "lot", "log"};
+
 		Solution solution = new Solution();
 		int answer = solution.solution(begin, target, words);
 		System.out.println("answer : " + answer);
